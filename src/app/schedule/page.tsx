9ebef87ccbe2d2a9c7dd4c686245cec083ddfb68
Mikita Slabysh <slabysh2015@gmail.com>
2025-10-11 22:00:32 -0400

    Began working on page outline

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,7 +1,22 @@
 export default function SchedulePage() {
   return (
     <main>
-      <h1>Schedule Page</h1>
+      <div id="header-container" className="fixed top-0 left-0 w-full h-16 bg-gray-900 text-white flex items-center justify-left px-4">
+        <h1 className="text-xl font-bold">Schedule Builder</h1>
+      </div>
+
+      <div id="schedule-container" className="rounded-lg shadow-lg p-4 m-4 mt-20 border border-gray-300">
+
+      </div>
+
+      <div id="class-selection-container" className="">
+
+      </div>
+
+      <div id="footer" className="absolute bottom-0 w-full h-12 bg-gray-800 text-white flex items-center justify-center">
+        <p className="text-sm">Developed by Group 6 CLD</p>
+      </div>
+      
       <a href="/">Go Home</a>
     </main>
   );
