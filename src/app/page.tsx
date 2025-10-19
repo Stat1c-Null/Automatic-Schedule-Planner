ee54b98155e20de617c491df47d69a70e0bebd54
atressler5 <atressler5@gmail.com>
2025-10-19 14:37:07 -0400

    made outline for HomePageContent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import NavBar from "@/components/NavBar";
+import HomePageContent from "@/components/HomePageContent";
 
 
 export default function HomePage() {
@@ -8,6 +9,9 @@ export default function HomePage() {
       <NavBar/>
       <h1>Home Page</h1>
       <a href="/schedule">Go to Schedule</a>
+
+
+      
     </main>
   );
 }
---
+++ src/components/HomePageContent.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface HomePageContentProps {
+  
+}
+
+export default function HomePageContent({
+  
+}: HomePageContentProps) {
+  return (
+    <div className="mx-4 bg-emerald-600">
+
+
+
+
+    </div>
+  );
+}
