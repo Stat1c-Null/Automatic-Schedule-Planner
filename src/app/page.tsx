8bad7dce45fa95fd9118b571526951a12192f088
atressler5 <atressler5@gmail.com>
2025-10-19 18:57:22 -0400

    Created divs in HomePageContent, created welcome and about sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
       <NavBar/>
       <h1>Home Page</h1>
       <a href="/schedule">Go to Schedule</a>
-
+      <HomePageContent/>
 
       
     </main>
--- src/components/HomePageContent.tsx
+++ src/components/HomePageContent.tsx
@@ -8,7 +8,24 @@ export default function HomePageContent({
   
 }: HomePageContentProps) {
   return (
-    <div className="mx-4 bg-emerald-600">
+    /*this div contains all of the content in HomePageContent, top margin to avoid NavBar overlapping*/
+    <div className="pt-6">
+    {/*creates background for content, welcome message*/}  
+    <div className="mx-4 bg-gray-800 text-yellow-300 rounded-sm px-8">
+      <h2 className="text-6xl" >Welcome!</h2>
+      <br></br>
+      <p className="text-xl">To get started, select the "Schedule" button on the top of the screen. To learn how to use, press the "Tutorial" button.</p>
+    </div>
+    
+    <br></br>
+    {/*About section*/}
+    <div className="mx-4 bg-gray-800 text-yellow-300 rounded-sm px-8">
+      <h2 className="text-6xl" >About this page</h2>
+      <p className="text-xl">This website will automatically generate schedules based on Rate My Professor ratings and the required classes in a degree.</p>
+      
+
+    </div>
+
 
 
 
