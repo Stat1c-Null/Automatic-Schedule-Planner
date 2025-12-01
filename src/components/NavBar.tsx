68aade8e4452212da34ea9d26ced29ecc56a1b60
atressler5 <atressler5@gmail.com>
2025-12-01 16:42:09 -0500

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -12,7 +12,7 @@ export default function NavBar(
   }: NavBarProps) {
   return (
     <div id="navbar" className="fixed top-0 left-0 w-full h-16 bg-gray-800 text-yellow-300 flex items-center justify-between px-4 gap-4 z-[9999] shadow-md">   
-        <div className="">
+        <div className="px-4">
           <h1 className="text-2xl font-bold">Automatic Schedule Planner</h1>
         </div>
         
