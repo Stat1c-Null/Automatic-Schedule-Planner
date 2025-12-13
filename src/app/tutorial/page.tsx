b07d28c484df89b4b82f6538f904e47e42be5b7e
atressler5 <atressler5@gmail.com>
2025-12-12 21:31:57 -0500

    made assets folder for images

--- src/app/tutorial/page.tsx
+++ src/app/tutorial/page.tsx
@@ -1,6 +1,7 @@
 ﻿"use client";
 import React from "react";
-import { useEffect } from "react";
+import { useEffect} from "react";
+import Image from "next/image";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 
@@ -13,10 +14,17 @@ export default function TutorialPage() {
         <main className='bg-black'>
             <NavBar/>
 
-            <div className="">
-                
-            </div>
+            {/*container for content*/}
+            <div className="pt-6 mt-16 bg-black">
+                <div className="margins colors p-5">
+                    <span className="text-3xl w-full mb-6">How to use</span>
+                    <br></br>
+                    <span className="text-xl w-full">First, enter all "Events". This will be all other activities such as work or clubs that interfere with scheduling.</span>
 
+
+                </div>
+
+            </div>
 
             <Footer/>
         </main>
