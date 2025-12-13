c97c9f5ea0c916618703751f6b0f6512f4d762b6
atressler5 <atressler5@gmail.com>
2025-12-12 19:41:03 -0500

    move features section in to HomePageContent

--- src/components/homepage/HomePageContent.tsx
+++ src/components/homepage/HomePageContent.tsx
@@ -14,6 +14,29 @@ export default function HomePageContent({
 
     <HomePageHeader/>
 
+        {/*creates background for content, welcome message*/}
+        <div className="margins colors">
+            {/*grid of features*/}
+            <div className="flex flex-wrap gap-2">
+
+                <div className="">
+                    <span>KSU Course Catalog Integration</span>
+                    <p>This app integrates with KSU's course catalog to provide lists of available classes given your desired schedule. Instead of searching manually, simply enter your classes and have our app do the work for you.</p>
+                </div>
+
+                <div className="">
+                    <span>Rate my Professor Integration</span>
+                    <p>This app integrates with Rate my Professor to easily select the top rated professors for your courses.</p>
+                </div>
+
+
+
+
+            </div>
+
+
+        </div>
+
     </div>
   );
 }
--- src/components/homepage/HomePageHeader.tsx
+++ src/components/homepage/HomePageHeader.tsx
@@ -16,25 +16,6 @@ export default function HomePageHeader({
       <br></br>
       <p className="text-3xl">Create your schedules in minutes instead of hours using our tool.</p>
 
-        {/*grid of features*/}
-        <div className="flex flex-wrap gap-2">
-
-            <div className="">
-                <span>KSU Course Catalog Integration</span>
-                <p>This app integrates with KSU's course catalog to provide lists of available classes given your desired schedule. Instead of searching manually, simply enter your classes and have our app do the work for you.</p>
-            </div>
-
-            <div className="">
-                <span>Rate my Professor Integration</span>
-                <p>This app integrates with Rate my Professor to easily select the top rated professors for your courses.</p>
-            </div>
-
-
-
-
-        </div>
-
-
     </div>
     
     <br></br>
